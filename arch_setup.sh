cb3720ccf561935bc47d6dd7bda813372de87a09
Diego Lira <diego.lira91@gmail.com>
2024-07-28 23:12:27 -0300

    bluetooth

--- arch_setup.sh
+++ arch_setup.sh
@@ -12,3 +12,9 @@ yay -S visual-studio-code-bin --noconfirm # VS Code directly from Microsoft
 
 yay -S onedrive-abraunegg --noconfirm # OneDrive client. Authenticate after setup: https://github.com/abraunegg/onedrive/blob/master/docs/USAGE.md
 
+
+## if bluetooth is not activated:
+systemctl status bluetooth
+
+sudo systemctl start bluetooth
+systemctl enable bluetooth
