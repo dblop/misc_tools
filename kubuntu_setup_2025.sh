de5099690451865cfd4a36672901c77d42952e0f
Diego Lira <diego.lira91@gmail.com>
2025-06-02 01:32:41 -0300

    some cmds

--- kubuntu_setup_2025.sh
+++ kubuntu_setup_2025.sh
@@ -12,9 +12,9 @@ sudo apt install -y cmake
 sudo apt install -y git
 
 sudo apt install -y qbittorrent
-snap install -y code
-snap install spotify
-
+sudo snap install -y code
+sudo snap install -y spotify
+sudo snap install -y steam
 
 #MPV installation
 sudo apt-get install devscripts equivs
@@ -22,3 +22,9 @@ git clone https://github.com/mpv-player/mpv-build.git && cd mpv-build
 mk-build-deps -s sudo -i # Install build dependencies
 ./rebuild -j8
 sudo ./install
+
+
+#Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
+sudo rm -f ./Miniconda3-latest-Linux-x86_64.sh
